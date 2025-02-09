772bcc71c71c6823641b9e180666399b821eada8
anjali <anjali.kodehode@gmail.com>
2025-02-09 21:02:11 +0100

    Second

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document
+  .getElementById("scroll-to-top")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
